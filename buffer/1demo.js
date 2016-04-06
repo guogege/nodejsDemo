e962ffacc0b4ca01d4f990a5aa4bcf5e53076500
guogege20102 <943522133@qq.com>
2016-04-06 17:09:15 +0800

    学习buffer

---
+++ buffer/1demo.js
@@ -0,0 +1,10 @@
+/**
+ * Created by zhang on 2016/1/10.
+ */
+var buf = new Buffer([65]);
+var buf = new Buffer([0x41])
+console.log(buf.toString());
+
+//var buf2=new Buffer(12);
+//console.log(buf2);
+//console.log(buf2[0]);
---
+++ buffer/2demo.js
@@ -0,0 +1,8 @@
+/**
+ * Created by zhang on 2016/1/10.
+ */
+var str="珠峰";
+console.log(str.length);
+var buf = new Buffer(str.length);
+
+console.log(buf.toString());
